aacc6d864d4a6d7177947c1bab8724791bd017ff
Sriram5-coder <saisriramkollepara@gmail.com>
2023-08-03 02:03:26 +0530

    basic html, css, js project

---
+++ tot.js
@@ -0,0 +1,28 @@
+function myFunction() {
+  var x = document.getElementById("fruit");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunction1() {
+  var y = document.getElementById("vegveg");
+  if (y.style.display === "none") {
+     y.style.display="block";
+  } else {
+    y.style.display="none";
+  }
+}
+// function myFunction2() {
+//   var z = document.getElementById("total");
+//   let a =document.getElementById("fruit");
+//   let b=document.getElementById("vegveg");
+//   if (z.style.display === "none") {
+//     z.style.display="block";
+//     a.style.display="none";
+//     b.style.display="none";
+//   } else {
+//     z.style.display = "none";
+//   }
+// }
